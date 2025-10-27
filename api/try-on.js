7f7e32d2479180c26b146acdc10bcf05010a4361
aigeneral-cyber <aigeneral@varchavsky.com.ar>
2025-10-27 20:51:07 -0300

    Update try-on.js

--- api/try-on.js
+++ api/try-on.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
     if (!humanImage || !garmentImage) {
       return res.status(400).json({ error: 'Missing image data.' });
     }
-    const model = "cuuupid/idm-vton:365b244557f36f08149d564476c5332f31f28b49237583765b293d08595d2c25";
+    const model = "cuuupid/idm-vton:0513734a452173b8173e907e3a59d19a36266e55b48528559432bd21c7d7e985";
     const input = {
       human_img: humanImage,
       garm_img: garmentImage,
